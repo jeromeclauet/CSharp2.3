e0f1b3893a5dc01a2aad24e15053acd049282c4a
jerome.clauet <clauet.jerome@hotmail.fr>
2021-01-16 12:25:14 +0100

    Exercice 4

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exercice_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Veuillez entrer un identifiant !");
+            string login = Console.ReadLine();
+            Console.WriteLine("Veuillez entrer un mot de passe !");
+            string password = Console.ReadLine();
+
+            switch(login)
+            {
+                case "jéjé":
+                    switch(password)
+                    {
+                        case "marabout":
+                            Console.WriteLine("Bienvenue à la Manu");
+                            break;
+                        default:
+                            Console.WriteLine("Identifiant ou mot de passe incorrect");
+                            break;
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Identifiant ou mot de passe incorrect");
+                    break;
+            }
+        }
+    }
+}
